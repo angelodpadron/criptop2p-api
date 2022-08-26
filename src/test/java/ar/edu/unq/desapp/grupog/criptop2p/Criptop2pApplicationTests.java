39ed5c6e07f7d653661b6ae554c7ff1dd6cadaa2
Angelo Padron <padron891@gmail.com>
2022-08-26 18:53:03 -0300

    initial commit

---
+++ src/test/java/ar/edu/unq/desapp/grupog/criptop2p/Criptop2pApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupog.criptop2p;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Criptop2pApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
