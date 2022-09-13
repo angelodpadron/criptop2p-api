da3efec4abac84c422650127e8d7e1329684bce0
Angelo Padron <padron891@gmail.com>
2022-09-13 18:32:45 -0300

    added user dto

--- src/main/java/ar/edu/unq/desapp/grupog/criptop2p/dto/UserDTO.java
+++ src/main/java/ar/edu/unq/desapp/grupog/criptop2p/dto/UserDTO.java
@@ -1,33 +1,32 @@
-package ar.edu.unq.desapp.grupog.criptop2p.model;
+package ar.edu.unq.desapp.grupog.criptop2p.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.NoArgsConstructor;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
-import javax.persistence.*;
-
-@AllArgsConstructor
-@NoArgsConstructor
-@Entity
-@Table(name = "users")
-public class User {
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private long id;
+public class UserDTO {
+    @Size(min = 3, max = 30, message = "Invalid name format. The length must be from 3 to 30 characters.")
+    @NotEmpty
     private String firstname;
+    @Size(min = 3, max = 30, message = "Invalid last name format. The length must be from 3 to 30 characters.")
+    @NotEmpty
     private String lastname;
+    @Email(message = "Invalid email format.")
+    @NotEmpty
     private String email;
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "The password must contain at least an uppercase, a lowercase, a number and a special character")
+    @NotEmpty
     private String password;
+    @Size(min = 10, max = 30)
+    @NotEmpty
     private String address;
+    @Pattern(regexp = "^\\d{22}$", message = "Invalid CVU format. The CVU consists of a 22 digit number.")
+    @NotEmpty
     private String cvuMercadoPago;
+    @Pattern(regexp = "^\\d{8}$", message = "Invalid wallet address format. The wallet address consists of a 8 digit number.")
+    @NotEmpty
     private String walletAddress;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
 
     public String getFirstname() {
         return firstname;
--- src/main/java/ar/edu/unq/desapp/grupog/criptop2p/webservices/UserController.java
+++ src/main/java/ar/edu/unq/desapp/grupog/criptop2p/webservices/UserController.java
@@ -1,7 +1,9 @@
 package ar.edu.unq.desapp.grupog.criptop2p.webservices;
 
+import ar.edu.unq.desapp.grupog.criptop2p.dto.UserDTO;
 import ar.edu.unq.desapp.grupog.criptop2p.model.User;
 import ar.edu.unq.desapp.grupog.criptop2p.service.UserService;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,11 +18,19 @@ public class UserController {
     @Autowired
     private UserService userService;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @PostMapping(path = "/create")
     @ResponseBody
-    public ResponseEntity<User> createUser(@Valid @RequestBody User user) {
-        User userCreated = userService.create(user);
+    public ResponseEntity<User> createUser(@Valid @RequestBody UserDTO userDTO) {
+        User userEntity = toUserEntity(userDTO);
+        User userCreated = userService.create(userEntity);
         return new ResponseEntity<>(userCreated, HttpStatus.CREATED);
+    }
+
+    private User toUserEntity(UserDTO userDTO) {
+        return modelMapper.map(userDTO, User.class);
     }
 
 }
